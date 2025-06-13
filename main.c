3f7c37b7eeef5dd35517991b50355ce7173e843b
Your Name <you@example.com>
2025-06-12 21:45:54 -0400

    Menus del programa

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+void mostrarMenuPrincipal() {
+    limpiarPantalla();
+    puts("========================================");
+    puts("            MMORPG NO LINEAL          ");
+    puts("========================================");
+  
+    puts("1) JUGAR EN SOLITARIO");
+    puts("2) JUGAR MULTIJUGADOR");
+    puts("3) SALIR");
+}
+
+
 int main(){
     printf("hello world");
     return 0;
